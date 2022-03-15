a2312e8c78e4ab5e1f062fe829e54bec8ec48dc2
michaelrios2015 <michael.rios093@gmail.com>
2022-03-14 20:42:05 -0400

    more platinums stuff

---
+++ cvs_readers/platinums.py
@@ -0,0 +1,90 @@
+import csv
+from datetime import datetime
+import psycopg2
+
+# connects to database
+conn = psycopg2.connect(
+    database="fannies", user='postgres',
+    password='JerryPine', host='localhost', port='5432'
+)
+
+# change this monthly will they always be named for this
+data_path = ['cvs_readers/data/input/collateral_items_giant_FHR.csv',
+             'cvs_readers\data\input\collateral_items_mega_FNM.csv',
+             'cvs_readers\data\input\collateral_items_super_FHR.csv',
+             'cvs_readers\data\input\collateral_items_super_FNM.csv']
+
+
+for file in data_path:
+
+    filename = file[-13:-4]
+
+    if filename[0] == '_':
+        filename = filename[1:]
+
+    print(filename)
+
+    with open(file, newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        # just skips the head I believe
+        headers = next(data)
+
+        body = []
+
+        for row in data:
+
+            try:
+                platcusip = row[0]
+                poolcusip = row[3]
+                ofinplat = row[9]
+
+                # print(date)
+
+                body.append([platcusip, poolcusip, ofinplat, filename])
+
+            except Exception as e:
+                # we seem to get a couple of very new supers (like platinums) each month
+                print(row)
+                print(e)
+
+    headfields = ["platcusip", "poolcusip", "ofinplat", "filename"]
+
+    with open('cvs_readers/data/output/' + filename, 'w', newline='') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile)
+
+        # writing the fields
+        csvwriter.writerow(headfields)
+
+        # writing the data rows
+        csvwriter.writerows(body)
+
+
+# connecting to database
+# what is autocommit
+conn.autocommit = True
+cursor = conn.cursor()
+
+csv_file_name = 'cvs_readers/data/output/giant_FHR'
+sql = "COPY platinums FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+
+csv_file_name = 'cvs_readers/data/output/mega_FNM'
+sql = "COPY platinums FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+
+csv_file_name = 'cvs_readers/data/output/super_FHR'
+sql = "COPY platinums FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+
+csv_file_name = 'cvs_readers/data/output/super_FNM'
+sql = "COPY platinums FROM STDIN DELIMITER ',' CSV HEADER"
+cursor.copy_expert(sql, open(csv_file_name, "r"))
+
+
+# not sure if these are ncessary but I think they help
+conn.commit()
+conn.close()
